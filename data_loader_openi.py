a0d0f62d1db63c14c8f56cd8e55a947f48d4f3bd
danenigma <danielfekaduenigma@yahoo.com>
2018-04-27 14:37:10 +0300

    divid data in two

--- data_loader_openi.py
+++ data_loader_openi.py
@@ -31,14 +31,14 @@ class OpenIDataset(data.Dataset):
 		self.transform = transform
 		self._vocab    = vocab 
 		self._features = torch.load(feature_path)
-		
+	
 	def __len__(self):
 		return len(self.table)
 	def _build_index(self):
 		imageName_to_feature_idx = {}
-        for idx, name in enumerate(self._features[0]):
-            imageName_to_feature_idx[name] = idx
-        self._imageName_to_feature_idx = imageName_to_feature_idx
+		for idx, name in enumerate(self._features[0]):
+			imageName_to_feature_idx[name] = idx
+		self._imageName_to_feature_idx = imageName_to_feature_idx
 
 	def _get_caption_tensor(self, caption):
 		vocab = self._vocab
@@ -49,14 +49,14 @@ class OpenIDataset(data.Dataset):
 		target.append(vocab('<end>'))
 		target = torch.Tensor(target)
 		return target
-	
+
 	def __getitem__(self, idx):
 		img_name = self.table.iloc[idx, 0]
 		fullname = img_name+'.png'
-		
+	
 		feature_idx = self._imageName_to_feature_idx[fullname]
 		feature     = self._features[feature_idx]
-		
+	
 		caption = self.table.iloc[idx, 3]
 		caption = self._get_caption_tensor(caption)
 
@@ -72,30 +72,30 @@ class OpenIDataset(data.Dataset):
 		lengths = [len(cap) for cap in captions]
 		targets = torch.zeros(len(captions), max(lengths)).long()
 		for i, cap in enumerate(captions):
-		    end = lengths[i]
-		    targets[i, :end] = cap[:end]
+			end = lengths[i]
+			targets[i, :end] = cap[:end]
 
 		return features, targets, lengths
 		
 def get_transform():
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    transform = transforms.Compose([transforms.ToTensor(), normalize])
-    return transform
+	normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+	transform = transforms.Compose([transforms.ToTensor(), normalize])
+	return transform
 
 
 def get_loader(vocab, feature_path, batch_size=1,
                shuffle=True, num_workers=2, name='train', transform=None):
 
-    openi = OpenIDataset(vocab,
-                         feature_path=feature_path,
-                         name=name, transform=transform)
+	openi = OpenIDataset(vocab,
+		                 feature_path=feature_path,
+		                 name=name, transform=transform)
 
-    data_loader = torch.utils.data.DataLoader(dataset=openi,
-                                              batch_size=batch_size,
-                                              shuffle=shuffle,
-                                              num_workers=num_workers,
-                                              collate_fn=openi.collate_fn)
-    return data_loader
+	data_loader = torch.utils.data.DataLoader(dataset=openi,
+		                                      batch_size=batch_size,
+		                                      shuffle=shuffle,
+		                                      num_workers=num_workers,
+		                                      collate_fn=openi.collate_fn)
+	return data_loader
 
 
 if __name__=='__main__':
@@ -113,12 +113,12 @@ if __name__=='__main__':
 	captions.extend(val_captions)
 	vocab = build_vocab_openi(captions, threshold=0)
 
-	
-	
+
+
 	loader    = get_loader(vocab, feature_path, batch_size=2,
-                shuffle=True, num_workers=2, name='train', transform=transform) 
+		        shuffle=True, num_workers=2, name='train', transform=transform) 
 	print('data loading done!!!!!!!')
 	for i, (feats, targets, lengths) in enumerate(loader):
 		print(feats.shape)
 		break	
-	
+
